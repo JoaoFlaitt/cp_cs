a164ea83576efff9b950a918a9dc901e1cdd379b
Joao Flaitt <143190074+JoaoFlaitt@users.noreply.github.com>
2025-09-08 22:02:42 -0300

    Add files via upload

---
+++ cp/cp_cs/MediaNotas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o nome do estudante:");
+        string nome = Console.ReadLine();
+
+        double soma = 0;
+
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine("Digite a nota " + i + ":");
+            double nota = Convert.ToDouble(Console.ReadLine());
+            soma += nota;
+        }
+
+        double media = soma / 3;
+        Console.WriteLine("A média do estudante " + nome + " é " + media);
+    }
+}
